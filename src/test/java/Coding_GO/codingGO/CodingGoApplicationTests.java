af9c3615cab72c43f1c2c692ee45250f2138c0f3
C0ng_yun <s25069@gsm.hs.kr>
2025-12-01 22:57:22 +0900

    프로젝트 세팅

---
+++ src/test/java/Coding_GO/codingGO/CodingGoApplicationTests.java
@@ -0,0 +1,13 @@
+package Coding_GO.codingGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
