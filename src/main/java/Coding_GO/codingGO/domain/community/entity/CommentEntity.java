390e13e3d7323e85254990e7090aa11e6db42dc7
C0ng_yun <s25069@gsm.hs.kr>
2025-12-07 22:52:30 +0900

    add : 커뮤니티 삭제 api 구현

---
+++ src/main/java/Coding_GO/codingGO/domain/community/entity/CommentEntity.java
@@ -0,0 +1,45 @@
+package Coding_GO.codingGO.domain.community.entity;
+
+import Coding_GO.codingGO.domain.community.data.Community;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "post_id")
+    private CommunityEntity post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private CommentEntity parent_comment;
+
+    @Column(nullable = false,name = "content" , length = 255)
+    private String content;
+
+    @CreationTimestamp
+    @Column(nullable = false,name = "created_at", updatable = false)
+    private LocalDateTime create_at;
+
+
+}
---
+++ src/main/java/Coding_GO/codingGO/domain/community/service/impl/DeleteCommunityServiceImpl.java
@@ -0,0 +1,26 @@
+package Coding_GO.codingGO.domain.community.service.impl;
+
+
+import Coding_GO.codingGO.domain.community.entity.CommunityEntity;
+import Coding_GO.codingGO.domain.community.repository.CommunityRepository;
+import Coding_GO.codingGO.domain.community.service.DeleteCommunityService;
+import Coding_GO.codingGO.global.exception.ErrorCode;
+import Coding_GO.codingGO.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeleteCommunityServiceImpl implements DeleteCommunityService {
+    private final CommunityRepository communityRepository;
+
+    @Override
+    public void execute(Long postId) {
+        CommunityEntity community =communityRepository.findById(postId)
+                .orElseThrow(() -> new GlobalException(ErrorCode.COMMUNITY_NOT_FOUND_EXCEPTION));
+
+        communityRepository.delete(community);
+    }
+}
