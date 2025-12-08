a5c26afe8e0d2710ab0471425f37245b8d9a33bb
C0ng_yun <s25069@gsm.hs.kr>
2025-12-08 09:02:41 +0900

    update: 페이지네이션 기본값 처리를 서비스 계층으로이동

--- src/main/java/Coding_GO/codingGO/domain/community/presentation/data/request/GetCommunityListRequest.java
+++ src/main/java/Coding_GO/codingGO/domain/community/presentation/data/request/GetCommunityListRequest.java
@@ -3,13 +3,4 @@ package Coding_GO.codingGO.domain.community.presentation.data.request;
 public record GetCommunityListRequest(
         Integer page,
         Integer limit
-) {
-    public GetCommunityListRequest{
-        if(page == null || page < 1){
-            page= 0;
-        }
-        if(limit == null || limit < 1){
-            limit = 10;
-        }
-    }
-}
+) {}
--- src/main/java/Coding_GO/codingGO/domain/community/service/impl/GetCommunityServiceImpl.java
+++ src/main/java/Coding_GO/codingGO/domain/community/service/impl/GetCommunityServiceImpl.java
@@ -1,56 +1,60 @@
-package Coding_GO.codingGO.domain.community.service.impl;
+    package Coding_GO.codingGO.domain.community.service.impl;
 
-import Coding_GO.codingGO.domain.community.data.Community;
-import Coding_GO.codingGO.domain.community.entity.CommunityEntity;
-import Coding_GO.codingGO.domain.community.mapper.CreateCommunityMapper;
-import Coding_GO.codingGO.domain.community.mapper.GetCommunityMapper;
-import Coding_GO.codingGO.domain.community.presentation.data.projection.CommentCount;
-import Coding_GO.codingGO.domain.community.presentation.data.response.GetCommunityListResponse;
-import Coding_GO.codingGO.domain.community.repository.CommunityRepository;
-import Coding_GO.codingGO.domain.community.service.GetCommunityService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+    import Coding_GO.codingGO.domain.community.data.Community;
+    import Coding_GO.codingGO.domain.community.entity.CommunityEntity;
+    import Coding_GO.codingGO.domain.community.mapper.CreateCommunityMapper;
+    import Coding_GO.codingGO.domain.community.mapper.GetCommunityMapper;
+    import Coding_GO.codingGO.domain.community.presentation.data.projection.CommentCount;
+    import Coding_GO.codingGO.domain.community.presentation.data.response.GetCommunityListResponse;
+    import Coding_GO.codingGO.domain.community.repository.CommunityRepository;
+    import Coding_GO.codingGO.domain.community.service.GetCommunityService;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.cache.annotation.Cacheable;
+    import org.springframework.data.domain.Page;
+    import org.springframework.data.domain.Pageable;
+    import org.springframework.data.domain.PageRequest;
+    import org.springframework.stereotype.Service;
+    import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+    import java.util.List;
+    import java.util.Map;
+    import java.util.stream.Collectors;
 
-@Service
-@Transactional
-@RequiredArgsConstructor
-public class GetCommunityServiceImpl implements GetCommunityService {
+    @Service
+    @Transactional
+    @RequiredArgsConstructor
+    public class GetCommunityServiceImpl implements GetCommunityService {
 
-    private final CommunityRepository communityRepository;
-    private final GetCommunityMapper mapper;
+        private final CommunityRepository communityRepository;
+        private final GetCommunityMapper mapper;
 
 
-    @Override
-    @Cacheable(
-            value = "communityList",
-            key = "'page:' + #page + ':limit:' + #limit\"
-    )
-    @Transactional(readOnly = true)
-    public GetCommunityListResponse execute(Integer page, Integer limit) {
-        Pageable pageable = PageRequest.of(
-                page,
-                limit
-        );
-        Page<Community> communityPage =
-                communityRepository.findAllCommunity(pageable)
-                        .map(mapper::toDto);
+        @Override
+        @Cacheable(
+                value = "communityList",
+                key = "'page:' + #page + ':limit:' + #limit"
+        )
+        @Transactional(readOnly = true)
+        public GetCommunityListResponse execute(Integer page, Integer limit) {
 
-        Map<Long,Long> commentCount = communityRepository.countCommunityByPost()
-                .stream()
-                .collect(Collectors.toMap(CommentCount::postId, CommentCount::commentCount));
+            int pagesu = (page == null || page < 1) ? 0 : page;
+            int limitsu = (limit == null || limit < 1) ? 10 : limit;
 
-        communityPage.getContent().forEach(c -> {
-            c.setCommentCount(commentCount.getOrDefault(c.getPostId(), 0L).intValue());
-        });
-        return mapper.toCommunityListResponse(communityPage);
+            Pageable pageable = PageRequest.of(
+                    pagesu,
+                    limitsu
+            );
+            Page<Community> communityPage =
+                    communityRepository.findAllCommunity(pageable)
+                            .map(mapper::toDto);
+
+            Map<Long,Long> commentCount = communityRepository.countCommunityByPost()
+                    .stream()
+                    .collect(Collectors.toMap(CommentCount::postId, CommentCount::commentCount));
+
+            communityPage.getContent().forEach(c -> {
+                c.setCommentCount(commentCount.getOrDefault(c.getPostId(), 0L).intValue());
+            });
+            return mapper.toCommunityListResponse(communityPage);
+        }
     }
-}
