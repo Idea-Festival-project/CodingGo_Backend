40626aa88b014220d098bd073d771284e4559213
C0ng_yun <s25069@gsm.hs.kr>
2025-12-11 21:05:35 +0900

    add: Comment DTO 클래스 추가

---
+++ src/main/java/Coding_GO/codingGO/domain/community/data/Comment.java
@@ -0,0 +1,28 @@
+package Coding_GO.codingGO.domain.community.data;
+
+import Coding_GO.codingGO.domain.community.data.constant.CommunityCategory;
+import Coding_GO.codingGO.domain.community.entity.CommentEntity;
+import Coding_GO.codingGO.domain.community.entity.CommunityEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+    Long commentId;
+    CommunityEntity post;
+    UserEntity author;
+    CommunityCategory category;
+    CommentEntity parentComment;
+    String content;
+    LocalDateTime create_at;
+
+    @Builder.Default
+    private List<CommentEntity> babyComments = new ArrayList<>();
+}
