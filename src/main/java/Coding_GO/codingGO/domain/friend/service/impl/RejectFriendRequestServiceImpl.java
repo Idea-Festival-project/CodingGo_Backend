da1cf7eb79547b79fdf2bd28b9de8354fc4eb22e
C0ng_yun <s25069@gsm.hs.kr>
2025-12-22 12:07:41 +0900

    add: 친구 요청 거절 api 구현

---
+++ src/main/java/Coding_GO/codingGO/domain/friend/service/impl/RejectFriendRequestServiceImpl.java
@@ -0,0 +1,39 @@
+package Coding_GO.codingGO.domain.friend.service.impl;
+
+import Coding_GO.codingGO.domain.friend.data.constant.FriendshipStatus;
+import Coding_GO.codingGO.domain.friend.entity.FriendEntity;
+import Coding_GO.codingGO.domain.friend.presentation.data.response.GetFriendListResponse;
+import Coding_GO.codingGO.domain.friend.repository.FriendRepository;
+import Coding_GO.codingGO.domain.friend.service.RejectFriendRequestService;
+import Coding_GO.codingGO.global.exception.ErrorCode;
+import Coding_GO.codingGO.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RejectFriendRequestServiceImpl implements RejectFriendRequestService {
+
+    private final FriendRepository friendRepository;
+
+    @Override
+    public void execute(Long userId, Long friendId) {
+
+        FriendEntity friend = friendRepository.findById(friendId)
+                .orElseThrow(()-> new GlobalException(ErrorCode.FRIEND_NOT_FOUND));
+
+        if (!friend.getFriend().getUserId().equlas(userId)){
+            throw new GlobalException(ErrorCode.UNAUTHORIZED_EXCEPTION);
+        }
+        if (friend.getStatus() == FriendshipStatus.ACCEPTED){
+            throw new GlobalException(ErrorCode.ALREADY_FRIENDS);
+        }
+        if (friend.getStatus() == FriendshipStatus.PENDING){
+            throw new GlobalException(ErrorCode.INVALID_STATUS);
+        }
+
+        friendRepository.save(friend);
+    }
+}
