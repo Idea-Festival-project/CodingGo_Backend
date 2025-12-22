75654f043bf568e41d19af82824de2f55522206b
C0ng_yun <s25069@gsm.hs.kr>
2025-12-22 12:09:13 +0900

    add: 받은 친구 목록 조회 api 구현

---
+++ src/main/java/Coding_GO/codingGO/domain/friend/mapper/GetSentFriendRequestListMapper.java
@@ -0,0 +1,32 @@
+package Coding_GO.codingGO.domain.friend.mapper;
+
+import Coding_GO.codingGO.domain.friend.data.GetPendingFriendRequestResponseItemDto;
+import Coding_GO.codingGO.domain.friend.data.constant.FriendshipStatus;
+import Coding_GO.codingGO.domain.friend.entity.FriendEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class GetSentFriendRequestListMapper {
+    public GetPendingFriendRequestResponseItemDto toGetSentFriendRequestResponseItemDto(FriendEntity friendEntity) {
+
+        UserEntity friendUser = friendEntity.getAuthor();
+
+        return GetPendingFriendRequestResponseItemDto.builder()
+                .friendId(friendEntity.getFriendshipId())
+                .username(friendUser.getUserId())
+                .username(friendUser.getUserName())
+                .nickname(friendUser.getNickname())
+                .profileImageUrl(friendUser.getProfileImageUrl())
+                .status(friendEntity.getStatus().equals(FriendshipStatus.PENDING))
+                .build();
+    }
+    public List<GetPendingFriendRequestResponseItemDto> toGetSentFriendRequestResponseItemDtoList(List<FriendEntity> friendEntities) {
+        return friendEntities.stream()
+                .map(this::toGetPendingFriendRequestResponseItemDto)
+                .collect(Collectors.toList());
+    }
+}
+}
