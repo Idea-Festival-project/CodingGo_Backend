466ae1b0c62cc9244e0f9cf49fd2c85dc711e548
C0ng_yun <s25069@gsm.hs.kr>
2025-12-22 13:34:05 +0900

    update: 세미콜론 추가

--- src/main/java/Coding_GO/codingGO/domain/friend/service/GetSentFriendRequestService.java
+++ src/main/java/Coding_GO/codingGO/domain/friend/service/GetSentFriendRequestService.java
@@ -3,5 +3,5 @@ package Coding_GO.codingGO.domain.friend.service;
 import Coding_GO.codingGO.domain.friend.presentation.data.response.GetPendingFriendRequestResponse;
 
 public interface GetSentFriendRequestService {
-    GetPendingFriendRequestResponse execute(Long userId, int page, int limit)
+    GetPendingFriendRequestResponse execute(Long userId, int page, int limit);
 }
