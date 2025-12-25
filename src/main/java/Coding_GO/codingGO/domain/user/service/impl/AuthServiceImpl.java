fc7fbc063e392e927db1c12ca668a7c711fc6a06
C0ng_yun <s25069@gsm.hs.kr>
2025-12-25 23:26:28 +0900

    update: 닉네임 중북 예외 추가

--- src/main/java/Coding_GO/codingGO/domain/user/service/impl/AuthServiceImpl.java
+++ src/main/java/Coding_GO/codingGO/domain/user/service/impl/AuthServiceImpl.java
@@ -39,6 +39,10 @@ public class AuthServiceImpl implements AuthService {
             throw new GlobalException(ErrorCode.AUTHENTICATION_NUMBER_MISMATCH);
         }
 
+        if(!userRepository.existsByNickname(request.nickname())) {
+            throw new GlobalException(ErrorCode.ALREADY_EXIST_NICKNAME);
+        }
+
         userRepository.save(UserEntity.builder()
                 .email(request.email())
                 .password(passwordEncoder.encode(request.password()))
